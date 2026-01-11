8e05d44a4b7fa93bf76f1fdfebe705626532caa9
Pankaj <pankaj.ky3007@gmail.com>
2026-01-12 01:47:15 +0530

    chore: nestjs code uploaded

---
+++ apps/api/src/users/users.service.ts
@@ -0,0 +1,32 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserRequest, UpdateUserRequest, User } from '@repo/schema';
+
+@Injectable()
+export class UsersService {
+  private readonly users: User[] = [];
+  create(createUserDto: CreateUserRequest) {
+    const user: User = {
+      ...createUserDto,
+      id: 1,
+    };
+
+    console.log(user);
+    return 'This action adds a new user';
+  }
+
+  findAll() {
+    return `This action returns all users`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserRequest) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
