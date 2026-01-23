552dfa9b73270fae76ab301e17e28086c8858832
Pankaj <pankaj.ky3007@gmail.com>
2026-01-23 13:06:27 +0530

    fix: authorization issue fixed

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -3,12 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ZodSerializerInterceptor, ZodValidationPipe } from 'nestjs-zod';
-import { APP_GUARD, APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
+import { APP_INTERCEPTOR, APP_PIPE } from '@nestjs/core';
 import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthModule } from './auth/auth.module';
-import { AuthGuard } from './auth/auth.guard';
-import { RolesGuard } from './auth/roles.guard';
 import { DomainModule } from './domain/domain.module';
 
 @Module({
@@ -32,15 +30,6 @@ import { DomainModule } from './domain/domain.module';
     {
       provide: APP_INTERCEPTOR,
       useClass: ZodSerializerInterceptor,
-    },
-    // AuthGuard must run BEFORE RolesGuard
-    {
-      provide: APP_GUARD,
-      useClass: AuthGuard,
-    },
-    {
-      provide: APP_GUARD,
-      useClass: RolesGuard,
     },
   ],
 })
