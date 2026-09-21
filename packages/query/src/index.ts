5cce56560aa6a12ba70480f1644483670afee66e
Pankaj <pankaj.ky3007@gmail.com>
2026-09-22 00:12:14 +0530

    refactor(query): simplify domain hooks to only useDomains

--- packages/query/src/index.ts
+++ packages/query/src/index.ts
@@ -1,6 +1,5 @@
 export * from "@tanstack/react-query";
 export * from "./mutation/use-auth";
-export * from "./mutation/use-domain";
 export * from "./query/use-domain";
 export * from "./mutation/use-users";
 export * from "./query/use-users";
