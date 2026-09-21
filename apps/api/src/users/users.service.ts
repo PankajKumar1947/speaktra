5b6b89d74a1dd5eb88d43338f4edc98520018f02
Pankaj <pankaj.ky3007@gmail.com>
2026-09-22 00:08:11 +0530

    refactor(api): update user entity and service to use domain enum

--- apps/api/src/users/users.service.ts
+++ apps/api/src/users/users.service.ts
@@ -17,18 +17,17 @@ export class UsersService {
   }
 
   findAll() {
-    return this.userModel.find().select('-password').populate('domain');
+    return this.userModel.find().select('-password');
   }
 
   findOne(id: string) {
-    return this.userModel.findById(id).select('-password').populate('domain');
+    return this.userModel.findById(id).select('-password');
   }
 
   update(id: string, updateUserDto: UpdateUserDto) {
     return this.userModel
       .findByIdAndUpdate(id, updateUserDto, { new: true })
-      .select('-password')
-      .populate('domain');
+      .select('-password');
   }
 
   remove(id: string) {
@@ -47,7 +46,6 @@ export class UsersService {
         },
         { new: true },
       )
-      .select('-password')
-      .populate('domain');
+      .select('-password');
   }
 }
